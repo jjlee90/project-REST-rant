e3c42a8e872ccf7164a35ab52193d1a59ffe8d09
jjlee90 <jlee190@hotmail.com>
2022-05-23 20:50:45 -0400

    Adding remaining stub routes

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,10 @@
+// making a controller
 const router = require('express').Router()
 
+// route for places
 router.get('/', (req, res) => {
     res.send('GET /places')
 })
 
+// exporting router 
 module.exports = router
--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+// get environment variables 
 require('dotenv').config()
+
+//require needed modules
 const express = require('express')
+
+// initialize the app object
 const app = express()
 
+// using router from places
 app.use('/places', require('./controllers/places'))
 
+// create home page route
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+// catch all other routes 
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
 
+// listen for connections 
 app.listen(process.env.PORT)
