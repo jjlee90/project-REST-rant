0fd14c8b1e605efc6fd3298f7821fe345de31938
jjlee90 <jlee190@hotmail.com>
2022-06-02 01:07:08 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,6 @@
 // making a controller
 const router = require("express").Router();
+const places = require("../models/places.js");
 
 // render new view
 router.get("/new", (req, res) => {
@@ -7,28 +8,22 @@ router.get("/new", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("POST /places");
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
 });
 
 // route for places
 router.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "/images/burger.jpg",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "/images/ramen.jpg",
-    },
-  ];
   res.render("places/index", { places });
 });
 
