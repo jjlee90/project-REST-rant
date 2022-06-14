0c44b2be91cb0246bdcc472242a6e5712462ea2f
jjlee90 <jlee190@hotmail.com>
2022-06-14 01:12:45 -0400

    added comments to show

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,21 @@ const React = require("react");
 const Def = require("../default");
 
 function show(data) {
+  let comments = <h3 className="inactive">No comments yet!</h3>;
+  if (data.place.comments.length) {
+    comments = data.place.comments.map((c) => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? "Rant! " : "Rave!"}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <strong>- {c.author}</strong>
+          </h3>
+          <h4>Raiting: {c.stars}</h4>
+        </div>
+      );
+    });
+  }
   return (
     <Def>
       <main>
@@ -45,7 +60,7 @@ function show(data) {
         <div className="comments">
           <hr />
           <h2 className="sectionHead">Comments</h2>
-          <h5>No Comments yet!</h5>
+          {comments}
         </div>
       </main>
     </Def>
